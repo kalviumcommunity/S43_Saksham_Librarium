69a168ed09be05204eb1a55babfd703c789e1876
Saksham091 <saksham.agarwal@kalvium.community>
2024-05-04 15:07:23 +0530

    Added Express app code to server.js along with /ping route

--- Server/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express")
-const auth = require("../Routes/auth")
-
-app.use(express.json())
-app.use("/auth", auth)
--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const express = require("express")
 const auth = require("./Routes/auth")
-require("dotenv").config()
 
-app.use(cors())
 app.use(express.json())
 app.use("/auth", auth)
-
-
-
