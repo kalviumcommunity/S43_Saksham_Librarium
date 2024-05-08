062eb686f337a8a0c46b403c72004875f8ea2432
Saksham091 <saksham.agarwal@kalvium.community>
2024-05-08 16:01:45 +0530

    Few Changes made

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const auth = require("./Routes/auth")
 
 app.use(express.json())
 app.use("/auth", auth)
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Now running on port ${process.env.PORT}`)
+})
+
+app.get("/", (req ,res) => {
+    res.send("Hi am working")
+})
